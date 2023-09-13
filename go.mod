5df8169d96a66be3738b2293787eed36483028e2
eatmeatball <abandon1a2b@outlook.com>
2023-09-13 22:50:16 +0800

    main:update

--- go.mod
+++ go.mod
@@ -9,15 +9,21 @@ require (
 )
 
 require (
+	github.com/gen2brain/beeep v0.0.0-20230907135156-1a38885a97fc // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -3,52 +3,125 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gen2brain/beeep"
+	"github.com/robfig/cron/v3"
 	"os"
 	"os/exec"
 	"sync"
+	"time"
 )
+
+const maxExecutionTime = 10 * time.Second // 最大允许的运行时间
+const maxConsecutiveFailures = 3          // 连续失败次数的最大值
 
 type Job struct {
 	JobName string   `json:"jobName"`
 	BinPath string   `json:"binPath"`
 	Params  []string `json:"params"`
 	Dir     string   `json:"dir"`
+	Timer   bool     `json:"timer"`
+	Spec    string   `json:"spec"`
 	Run     bool     `json:"run"`
 }
 
+type JobConfig struct {
+	ResidentTask  []Job `json:"residentTask"`
+	ScheduledTask []Job `json:"scheduledTask"`
+}
+
+func cockSay(msg string) {
+	err := beeep.Notify("Cock", msg, "")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
+
 	fileData, err := os.ReadFile("jobConfig.json")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	var jobList []Job
-	err = json.Unmarshal(fileData, &jobList)
+	var jobConfig JobConfig
+	err = json.Unmarshal(fileData, &jobConfig)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	go schedule(jobConfig.ScheduledTask)
+
 	wg := sync.WaitGroup{}
-	for _, job := range jobList {
+	counter := 0
+	for _, job := range jobConfig.ResidentTask {
 		wg.Add(1)
 		go func(job Job) {
 			defer wg.Done()
 			if job.Run == false {
 				return
 			}
-
-			cmd := exec.Command(job.BinPath, job.Params...)
-			cmd.Dir = job.Dir
-			cmd.Stdin = os.Stdin
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			cmdErr := cmd.Run()
-			if cmdErr != nil {
-				fmt.Println(cmdErr)
+			consecutiveFailures := 1
+			for {
+				startTime := time.Now()
+				cmd := exec.Command(job.BinPath, job.Params...)
+				cmd.Dir = job.Dir
+				cmd.Stdin = os.Stdin
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+				counter += 1
+				cmdErr := cmd.Run()
+				executionTime := time.Since(startTime)
+				if cmdErr != nil {
+					fmt.Println(cmdErr)
+				}
+				if executionTime <= maxExecutionTime {
+					consecutiveFailures += 1
+				} else {
+					consecutiveFailures = 1
+				}
+				if consecutiveFailures >= maxConsecutiveFailures {
+					cockSay(job.JobName + "程序连续3次启动失败，停止重启")
+					break
+				} else {
+					cockSay(job.JobName + "程序终止尝试重新运行")
+				}
 			}
-			fmt.Println("end")
 		}(job)
 	}
+	cockSay(fmt.Sprintf("cock启动成功"))
+
 	wg.Wait()
+	fmt.Println("当前没有任务")
+}
+
+func schedule(jobList []Job) {
+	var c = cron.New()
+	for _, job := range jobList {
+		if job.Run == false {
+			continue
+		}
+		if job.Spec == "" {
+			continue
+		}
+		_, err := c.AddFunc(job.Spec, func(job Job) func() {
+			return func() {
+				cmd := exec.Command(job.BinPath, job.Params...)
+				cmd.Dir = job.Dir
+				cmd.Stdin = os.Stdin
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+				cmdErr := cmd.Run()
+				if cmdErr != nil {
+					fmt.Println(cmdErr)
+				}
+				fmt.Println("end")
+			}
+		}(job))
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(job.JobName + "加入定时任务")
+	}
+	c.Run()
 }
