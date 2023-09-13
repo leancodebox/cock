0e7bd33648dace76e808870261cff94c10863bed
eatmeatball <abandon1a2b@outlook.com>
2023-09-13 21:30:26 +0800

    main:config

--- main.go
+++ main.go
@@ -1,21 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
+	"sync"
 )
 
-func main() {
-	cmd := exec.Command("thh", "serve")
+type Job struct {
+	JobName string   `json:"jobName"`
+	BinPath string   `json:"binPath"`
+	Params  []string `json:"params"`
+	Dir     string   `json:"dir"`
+	Run     bool     `json:"run"`
+}
 
-	cmd.Dir = "D:\\workspace\\cock"
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+func main() {
+	fileData, err := os.ReadFile("jobConfig.json")
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	fmt.Println("end")
+	var jobList []Job
+	err = json.Unmarshal(fileData, &jobList)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	wg := sync.WaitGroup{}
+	for _, job := range jobList {
+		wg.Add(1)
+		go func(job Job) {
+			defer wg.Done()
+			if job.Run == false {
+				return
+			}
+
+			cmd := exec.Command(job.BinPath, job.Params...)
+			cmd.Dir = job.Dir
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmdErr := cmd.Run()
+			if cmdErr != nil {
+				fmt.Println(cmdErr)
+			}
+			fmt.Println("end")
+		}(job)
+	}
+	wg.Wait()
 }
